943bee4f3e8fdf520e5099b94911f650c2a7ef07
RonaldRamosG <ronald1781@gmail.com>
2019-11-09 13:08:06 -0500

    Update conexdb2_login.php

--- application/config/conexdb2_login.php
+++ application/config/conexdb2_login.php
@@ -4,7 +4,7 @@
     $dserver=dato_hostas();
 $server=$dserver['ipas'];
     $dsn ="DRIVER={iSeries Access ODBC Driver};SYSTEM=".$server.";";
-    //$dsn ="DRIVER={iSeries Access ODBC Driver};SYSTEM=192.168.1.105;";
+    //$dsn ="DRIVER={iSeries Access ODBC Driver};SYSTEM=192.168.1.000;";
     $dbconect = odbc_connect($dsn, $usuario, $password);    
     if ($dbconect === false) {
        // $sqlerror = odbc_errormsg($dbconect);
